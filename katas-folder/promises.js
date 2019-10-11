9e6af75d11d0b258feff36e1b609167a3d26a92a
oscar leon <oscar.leon.delmar@gmail.com>
2019-10-11 00:15:03 -0500

    promises promises 1

--- katas-folder/promises.js
+++ katas-folder/promises.js
@@ -16,10 +16,37 @@
 // })
 
 
+// const userLeft = false
+// const userWatchingCatMeme = false
+
+// function watchTutorialCallback(callback, errorCallback) {
+//     if (userLeft) {
+//         errorCallback({
+//             name: 'User Left',
+//             message: ':('
+//         })
+//     } else if (userWatchingCatMeme) {
+//         errorCallback({
+//             name: 'User watching cat meme',
+//             message: 'web deb < cat'
+//         })
+//     } else {
+//         callback('thumbs up and subcribe')
+//     }
+// }
+
+// watchTutorialCallback((message) => {
+//     console.log('Success: ' + message);
+// },(error) => {
+//     console.log(error.name + ' ' + error.message);
+// }) 
+
+
 const userLeft = false
 const userWatchingCatMeme = false
 
-function watchTutorialCallback(callback, errorCallback) {
+function watchTutorialpromise() {
+    return new Promise((resolve, reject) => {
     if (userLeft) {
         errorCallback({
             name: 'User Left',
@@ -32,6 +59,42 @@ function watchTutorialCallback(callback, errorCallback) {
         })
     } else {
         callback('thumbs up and subcribe')
-    }
+    }  
+    })
+
 }
 
+watchTutorialpromise().then((message) => {
+    console.log('Success: ' + message);
+}).catch((error) => {
+    console.log(error.name + ' ' + error.message);
+})
+
+
+
+const recordVideoOne = new Promise((resolve, reject) => {
+    resolve('video 1 recorded')
+}) 
+
+const recordVideoTwo = new Promise((resolve, reject) => {
+    resolve('video 2 recorded')
+}) 
+
+const recordVideoThree = new Promise((resolve, reject) => {
+    resolve('video 3 recorded')
+}) 
+
+Promise.all([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+]).then((message) => {
+    console.log(message);
+}) 
+Promise.race([
+    recordVideoOne,
+    recordVideoTwo,
+    recordVideoThree
+]).then((message) => {
+    console.log(message);
+}) 
