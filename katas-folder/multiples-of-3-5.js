3d69efe589d1cfc86f157ea7f52174900dd4d11e
oscar leon <oscar.leon.delmar@gmail.com>
2019-12-23 20:29:18 -0600

    example a/b

--- katas-folder/multiples-of-3-5.js
+++ katas-folder/multiples-of-3-5.js
@@ -18,3 +18,22 @@ function solution(number) {
   }
   
 //   solution(10);
+
+function multiply (a, b) {
+  return a * b;
+}
+
+function timesCreator (a) {
+  return function (b) {
+      return multiply(a, b);
+  }
+}
+
+timesTwo = timesCreator(2);
+timesTwelve = timesCreator(12);
+
+// Outputs: 4
+console.log(timesTwo(2));
+
+// Outputs: 24
+console.log(timesTwelve(2));
